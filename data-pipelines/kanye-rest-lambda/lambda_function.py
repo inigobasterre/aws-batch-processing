0c8b42181713060cd9d0deb12deca73fbae6634a
Iñigo Basterretxea <inigo.basterretxea@uk.nationalgrid.com>
2024-04-16 11:44:28 +0100

    Add lambda function handler

--- data-pipelines/kanye-rest-lambda/lambda_function.py
+++ data-pipelines/kanye-rest-lambda/lambda_function.py
@@ -15,6 +15,14 @@ def get_and_print_api_data(url):
         print("Error fetching data:", e)
 
 
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
+
+
 if __name__ == "__main__":
     api_url = "https://api.kanye.rest/"
     get_and_print_api_data(api_url)
