bc988eda79a0a65a64338947494d516803e90de0
Iñigo Basterretxea <inigo.basterretxea@uk.nationalgrid.com>
2024-04-22 16:18:27 +0100

    Add private subnet

--- infra/networking.tf
+++ infra/networking.tf
@@ -1,9 +1,15 @@
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
 
   tags = merge({
     Name = "inigo-basterretxea-vpc"
+  }, var.tags)
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  tags = merge({
+    Name = "inigo-basterretxea-lambda-subnet"
   }, var.tags)
 }
