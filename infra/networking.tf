62005a14e87ca4aeaf227c3b232fb457c0e0ddd8
Iñigo Basterretxea <inigo.basterretxea@uk.nationalgrid.com>
2024-05-21 10:39:34 +0100

    Test CI/CD on Github Actions

--- infra/networking.tf
+++ infra/networking.tf
@@ -33,7 +33,7 @@ resource "aws_nat_gateway" "example" {
   depends_on = [aws_vpc.main]
 }
 
-resource "aws_security_group" "example" {
+resource "aws_security_group" "test" {
   vpc_id = aws_vpc.main.id
   description = "Security group to allow outbound from the VPC"
   depends_on = [aws_vpc.main]
